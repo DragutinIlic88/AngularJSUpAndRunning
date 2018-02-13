bb4c502e592e50d484343043dd2677b529e6c513
Dragutin Ilic <dragutinilic88@gmail.com>
2018-02-13 19:58:33 +0100

    Examples with mocking  service

---
+++ chapter7/serverAppSpec.js
@@ -0,0 +1,24 @@
+describe('MainCtrl Server Calls', function(){
+    beforeEach(module('serverApp'));
+
+    var ctrl, mockBackend;
+
+    beforeEach(inject(function($controller, $httpBackend){
+        mockBackend = $httpBackend;
+        mockBackend.expectGET('/api/note')
+            .respond([{id:1,label: 'Mock'}]);
+        ctrl = $controller('MainCtrl');
+    }));
+
+    it('should load items from server', function(){
+        expect(ctrl.items).toEqual([]);
+
+        mockBackend.flush();
+        expect(ctrl.items).toEqual([{id: 1, label: 'Mock'}]);
+    });
+
+    afterEach(function(){
+        mockBackend.verifyNoOutstandingExpectation();
+        mockBackend.verifyNoOutstandingRequest();
+    })
+})
